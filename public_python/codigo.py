1751cd0f33e452346135c07ce4af8a5723f7920e
lookfire <renancontiero@hotmail.com>
2020-02-11 23:09:04 -0300

    add main.py

---
+++ public_python/codigo.py
@@ -0,0 +1,11 @@
+class codigo:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+    def status(self):
+        print(f'Meu nome é {self.nome} e tenho {self.idade} anos')
+
+
+
+
---
+++ public_python/main.py
@@ -0,0 +1,8 @@
+from codigo import codigo
+
+p1 = codigo('renan', 25)
+p1.status()
+
+
+
+
